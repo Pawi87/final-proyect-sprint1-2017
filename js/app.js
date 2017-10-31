d7c0afffa199293629fe0a76b00fbf738bf595f4
Pawi87 <arellanopaulina12@gmail.com>
2017-10-31 00:52:04 -0600

    continue with comments in app.js

--- js/app.js
+++ js/app.js
@@ -17,7 +17,7 @@ function cipher(wordUser) {
             // si el codigo ascii, es mayor a 97 al 122 se encuentran las letras minusculas ya que 97 corresponde a la a minuscula en el codigo Ascii y 122 es el valor de la letra z en minuscula
 
             crypted += String.fromCharCode((letters - 97 + 33) % 26 + 97);
-            // para las minusculas se tomaran los datos del codigo de Ascii con el valor de 97 sumarle el modulo de 33 que son los espacios a la derecha del abc, modulo de 26, su total da a 7 + 97 
+            // para las minusculas se tomaran los datos del codigo de Ascii con el valor de 97 sumarle el modulo de 33 que son los espacios a la derecha del abc, modulo de 26, su total da a 7 + 97. Se ocupa String para extraer las letras de los codigos ascii, el resultado de la operacion es un numero y tengo que transformarlo a letras por eso se ocupa el String.
         } else {
             crypted += String.fromCharCode((letters - 65 + 33) % 26 + 65);
             //para las mayusculas se tomaran los datos del codigo de Ascii que corresponde a la A mayuscula a 65 
@@ -47,7 +47,7 @@ function decipher(crypted) {
             //si letters es mayor o igual a 97 que corresponde al valor de a minuscula, y que la variable de lettersCode sea mayor o igual que el 122 que es la z en minuscula
 
             word += String.fromCharCode((lettersCode - 97 - 33 + 26) % 26 + 97);
-            //para las minusculas se tomaran los datos del codigo de Ascii con el valor de 97 menos el modulo de 33 que son los espacios a la derecha del abc, modulo de 26 más 97
+            //para las minusculas se tomaran los datos del codigo de Ascii con el valor de 97 menos el modulo de 33 que son los espacios a la derecha del abc, modulo de 26 más 97. Se resta por que ahora se esta regresando los espacios que se avanzaron.
 
         } else {
             word += String.fromCharCode((letterCode - 65 - 33 + 26) % 26 + 65);
